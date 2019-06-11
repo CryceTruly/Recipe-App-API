4cacfc8f70ca32d940ff7567ec8d1573807cae77
crycetruly <crycetruly@gmail.com>
2019-06-11 22:18:40 +0300

    ft(mocks): add waiting for db

--- app/core/models.py
+++ app/core/models.py
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_super_user(self, email, password):
+    def create_superuser(self, email, password):
         """Creates aand saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
